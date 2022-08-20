2d266f4a19b40bf5b1de9037bca2a56d43d87d72
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-08-20 21:37:46 +0300

    commit tree 633ab3484d4474358c2e94ec7d75f9601b11dd3b

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,9 @@
 # Set up Bundler here following the instructions in the README
+require 'bundler/setup'
+Bundler.require(:default, :development)
+
+
+array = [1,2,3,4]
+hash = {:a => 1, :b => 5}
+ap array
+ap hash
